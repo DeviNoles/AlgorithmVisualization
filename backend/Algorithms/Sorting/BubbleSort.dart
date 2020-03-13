ec30dd54af74fc88589033fdcaaca93d9eed1c51
DevinM6 <devin8821@gmail.com>
2020-03-13 19:50:30 -0400

    Connected angel to redis

--- backend/Algorithms/Sorting/BubbleSort.dart
+++ backend/Algorithms/Sorting/BubbleSort.dart
@@ -1,5 +1,10 @@
+import 'package:angel_redis/angel_redis.dart';
+import 'package:resp_client/resp_client.dart';
+import 'package:resp_client/resp_commands.dart';
+
 class BubbleSort {
   List ar;
+  int count = 0;
   BubbleSort(List ar){
      this.ar = ar;
    }
@@ -8,17 +13,42 @@ class BubbleSort {
       print(type);
   }
 }
+void pushToRedis() async{
+  var connection = await connectSocket('192.168.0.100');
+  var client = new RespClient(connection);
+  var service = new RedisService(new RespCommands(client), prefix: 'bubblesort');
+
+  // Create an object
+  await service.create({'id': 'a', 'hello': 'world'});
+
+  // Read it...
+  var read = await service.read('a');
+  print(read['hello']);
+
+  // Delete it.
+
+  // Close the connection.
+  await connection.close();
+}
+
   List sort(){
+    List stringPairs = new List();
     for(int i = 0; i<this.ar.length; i++){
+
       for(int j=0; j<this.ar.length-i-1; j++){
         if(this.ar[j]>this.ar[j+1]){
           int holder = this.ar[j];
-          this.ar[j] = this.ar[j+1];
-          this.ar[j+1] = holder;
+         this.ar[j] = this.ar[j+1];
+         this.ar[j+1] = holder;
+         stringPairs.add(i.toString() + "," + j.toString());
         }
       }
 
     }
+    for(int i = 0; i<stringPairs.length; i++){
+      print(stringPairs[i]);
+    }
+    pushToRedis();
     printArray();
     return this.ar;
   }
