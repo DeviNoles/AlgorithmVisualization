cbfe015ee51a73408c0a2a35bbc4666a6b85283c
DevinM6 <devin8821@gmail.com>
2020-03-15 16:10:02 -0400

    Sort Animation! 📈

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Node from './node/Node.js';
 import './App.css';
 import random from './logic/generateRandom';
-
+var key = 0;
 class App extends Component{
     /* Values are hard-coded for this example, it's usually best to bring these in via file or environment variable for production */
 
@@ -16,6 +16,7 @@ class App extends Component{
   }
 async insertRedis(numAr) {
     var ns = this.state.nums;
+    let argText;
     const nums = {
      nums: ns
    };
@@ -26,15 +27,16 @@ async insertRedis(numAr) {
       })
       await axios.get(`http://192.168.0.101:6921/getRedis`)
       .then(async res => {
-          await this.getRedis(res.request.responseText);
-          console.log(res);
+          argText = res.request.responseText;
+          console.log(argText);
         //  console.log(res.request.responseText);
         })
 
+        this.getRedis(argText);
 }
 
 async getRedis(res){
-
+  console.log(res);
     let count = 0;
     let buffer = "";
     for (var i = 0; i < res.length; i++) {
@@ -42,18 +44,28 @@ async getRedis(res){
         continue;
       }
       else {
-        this.state.nums[i+1] = res.charAt(i);
-        this.state.nums[i] = res.charAt(i+2);
-        console.log(res.charAt(i) + res.charAt(i+2));
+      //  console.log(this.state.nums[parseInt(res.charAt(i))] + '=' + res.charAt(i+2));
+      //  console.log(this.state.nums[parseInt(res.charAt(i+2))]+ '=' + res.charAt(i));
+        let holder = this.state.nums[parseInt(res.charAt(i))];
+        console.log('Holder is: ' + holder);
+        this.state.nums[parseInt(res.charAt(i))] = this.state.nums[parseInt(res.charAt(i+2))];
+        this.state.nums[parseInt(res.charAt(i+2))] = holder;
+        console.log(holder + "<" + this.state.nums[parseInt(res.charAt(i))]);
         i = i+2;
+        key++;
+        const sleep = (milliseconds) => {
+          return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+  await  sleep(100).then(() => {
+    this.forceUpdate();
+    })
 
-        this.forceUpdate();
       }
   }
 }
 
   initializeGraph() {
-      for(let i = 0; i<4; i++){
+      for(let i = 0; i<10; i++){
         this.state.nums[i] = random();
           console.log(this.state.nums[i]);
       }
@@ -61,7 +73,6 @@ async getRedis(res){
     }
   render(){
     console.log('here');
-
     return (
     <div id="nodeBackground">
   <div className="container">
@@ -70,7 +81,7 @@ async getRedis(res){
   <div id="menu">
   Algorithm Visualization
   </div>
-  <div id="nodeContainer">
+  <div  key ={key} id="nodeContainer">
 
   {this.state.nums.map(function(height, index){
     return(
@@ -78,6 +89,7 @@ async getRedis(res){
     <Node  background = "blue" height = {height}/>
     </React.Fragment>
   )
+  {index++};
   })}
 
   </div>
